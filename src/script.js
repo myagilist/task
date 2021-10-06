e7955edc867bd5e9a1f86a0c7048f7fd2b5f9963
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-10-06 04:42:45 +0200

    remove basics

--- src/script.js
+++
@@ -1,7 +0,0 @@
-import Person from "./Person.ts";
-import './style.scss';
-
-let albus = new Person("Albus", "Dumbledore");
-let wrapper = document.getElementById('wrapper');
-
-wrapper.innerHTML = `<p>${albus.message()}</p>`;
