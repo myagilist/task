fbd19a1fcdb623c445c77dea05259763a45350f0
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-10-06 15:10:07 +0200

    add custom hook

--- src/TodoListHooks.js
+++ src/TodoListHooks.js
@@ -3,16 +3,36 @@ import { render } from 'react-dom';
 
 import './style.scss';
 
+let counter = 0;
+const levels = ['lowest', 'low', 'medium', 'high'];
+
+function usePriority(current) {
+  const [level, setLevel] = useState(current);
+
+  const modifyLevel = () => setLevel(l => { 
+    if (l < (levels.length - 1)) { 
+      return l + 1 
+    } else { 
+      return 0 
+    }
+  }) 
+    
+  return [level, modifyLevel];
+}
+
 function Task(props) {
+  const [currentPriority, changePriority] = usePriority(0);
+
   return (
-    <div className="todo">
+    <div key={props.counter} className="todo" onClick={changePriority}>
       <p>{props.message}</p>
+      <span className={`badge badge-${levels[currentPriority]}`}>{levels[currentPriority]}</span>
     </div>
   )
 }
 
 function TodoList() {
-  const [currentMessage, setMessage] = useState(null);
+  const [currentMessage, setMessage] = useState('');
   const [list, addTodo] = useState([]);
 
   const handleMessage = e => {
@@ -20,7 +40,8 @@ function TodoList() {
   }
 
   const handleTodo = () => {
-    addTodo([...list, <Task message={currentMessage} />])
+    counter += 1;
+    addTodo([...list, <Task key={counter} message={currentMessage} />])
   }
 
   return (
